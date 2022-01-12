e907c925c433b8803b510b0e6a76e2cb629ea2cb
Lokdao <52272454+lokdao@users.noreply.github.com>
2022-01-12 12:01:36 +0800

    removed commented code under cipher.hs

--- chapter09/cipher.hs
+++ chapter09/cipher.hs
@@ -9,14 +9,12 @@ import Data.Char
 --        Text to cipher
 -- Output: Ciphered text
 caesar :: Int -> String -> String
---caesar n xs = map (chr . ((+n) . ord)) xs
 caesar n = substitute (+n)
 
 -- Input: Offset used to cipher the text
 --        Text to decipher
 -- Output: Ciphered text
 unCaesar :: Int -> String -> String
---unCaesar n xs = map (chr . (subtract n . ord)) xs
 unCaesar n = substitute (subtract n)
 
 ------------------------
